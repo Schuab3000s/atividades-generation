abe53b5f7d2cf18c4c678ef56a1c1ae96b6b8e33
Kevin Schuab <kevinschuab@gmail.com>
2024-10-28 13:31:58 -0300

    add a atividade 3

---
+++ gen_exercises/src/atividades/dia_6/atividade_3.java
@@ -0,0 +1,26 @@
+package atividades.dia_6;
+
+import java.util.Scanner;
+
+public class atividade_3 {
+	public static void main(String[] args) {
+
+		float salarioBruto, addNoturno, hrsExtras, desconto, salarioLiquido;
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Digite o Salário Bruto: ");
+		salarioBruto = entrada.nextFloat();
+		System.out.println("Digite o Adicional Noturno: ");
+		addNoturno = entrada.nextFloat();
+		System.out.println("Digite as Horas Extras: ");
+		hrsExtras = entrada.nextFloat();
+		System.out.println("Digite os Descontos: ");
+		desconto = entrada.nextFloat();
+		entrada.close();
+
+		salarioLiquido = (salarioBruto + addNoturno + (hrsExtras * 5) - desconto);
+
+		System.out.println("Seu salário lpiquido é: " + salarioLiquido);
+	}
+}
