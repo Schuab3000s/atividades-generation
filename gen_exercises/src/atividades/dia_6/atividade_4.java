f5c079e114972c872fe41df9f0e124f6514a7c61
Kevin Schuab <kevinschuab@gmail.com>
2024-10-28 13:32:08 -0300

    add a atividade 4

---
+++ gen_exercises/src/atividades/dia_6/atividade_4.java
@@ -0,0 +1,26 @@
+package atividades.dia_6;
+
+import java.util.Scanner;
+
+public class atividade_4 {
+	public static void main(String[] args) {
+
+		float num1, num2, num3, num4, resultado;
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Digite o número 1: ");
+		num1 = entrada.nextFloat();
+		System.out.println("Digite o número 2: ");
+		num2 = entrada.nextFloat();
+		System.out.println("Digite o número 3: ");
+		num3 = entrada.nextFloat();
+		System.out.println("Digite o número 4: ");
+		num4 = entrada.nextFloat();
+		entrada.close();
+		
+		resultado = ((num1*num2)-(num3*num4));
+		
+		System.out.println("\nO resultado da diferença do produto entre o n1 e n2 pelo produto entre o n3 e o n4 é: "+ resultado);
+	}
+}
