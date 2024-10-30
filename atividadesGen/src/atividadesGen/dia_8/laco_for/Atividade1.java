2243484f2a92ec5b5559a4340f78e5b36bfcd6e5
Kevin Schuab <kevinschuab@gmail.com>
2024-10-30 14:24:45 -0300

    add atividades referente ao dia 8 sobre laços condicionais for

---
+++ atividadesGen/src/atividadesGen/dia_8/laco_for/Atividade1.java
@@ -0,0 +1,30 @@
+package atividadesGen.dia_8.laco_for;
+
+import java.util.Scanner;
+
+public class Atividade1 {
+	public static void main(String[] args) {
+
+		int num1, num2;
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro número do intervalo: ");
+		num1 = entrada.nextInt();
+		System.out.println("Digite o segundo número do intervalo: ");
+		num2 = entrada.nextInt();
+		System.out.println("");
+
+		if (num1 < num2) {
+			for (num1++; num1 < num2; num1++) {
+
+				if (num1 % 3 == 0 && num1 % 5 == 0) {
+					System.out.println(num1 + " é múltiplo de 3 e 5.");
+				}
+
+			}
+		} else {
+			System.out.println("\nIntervalo Inválido");
+		}
+	}
+}
