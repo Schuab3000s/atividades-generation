57dd92f39d57e4bfe0e135b3e961391577de66e9
Kevin Schuab <kevinschuab@gmail.com>
2024-10-30 14:24:57 -0300

    add atividades referente ao dia 8 sobre laços condicionais while

---
+++ atividadesGen/src/atividadesGen/dia_8/laco_while/Atividade1.java
@@ -0,0 +1,24 @@
+package atividadesGen.dia_8.laco_while;
+
+import java.util.Scanner;
+
+public class Atividade1 {
+	public static void main(String[] args) {
+		int idade = 0, menor21 = 0, maior50 = 0;
+
+		Scanner entrada = new Scanner(System.in);
+
+		while (!(idade < 0)) {
+			System.out.println("Digite uma idade: ");
+			idade = entrada.nextInt();
+
+			if (idade < 21 && idade>0) {
+				menor21++;
+			} else if (idade > 50) {
+				maior50++;
+			}
+		}
+		System.out.println("\nTotal de pessoas menores de 21 anos: " + menor21);
+		System.out.println("Total de pessoas maior de 50 anos: " + maior50);
+	}
+}
