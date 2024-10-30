f1687818ab61d468417fd0ef050edd0b1df1e08d
Kevin Schuab <kevinschuab@gmail.com>
2024-10-30 14:25:08 -0300

    add atividades referente ao dia 8 sobre laços condicionais do-while

---
+++ atividadesGen/src/atividadesGen/dia_8laco_do_while/Atividade1.java
@@ -0,0 +1,23 @@
+package atividadesGen.dia_8laco_do_while;
+
+import java.util.Scanner;
+
+public class Atividade1 {
+	public static void main(String[] args) {
+
+		int numero = 0, somaNumPositivo = 0;
+
+		Scanner entrada = new Scanner(System.in);
+
+		do {
+			System.out.println("Digite um número:");
+			numero = entrada.nextInt();
+
+			if (numero > 0) {
+				somaNumPositivo = somaNumPositivo + numero;
+			}
+		} while (numero != 0);
+
+		System.out.println("A soma dos números positivos é: " + somaNumPositivo);
+	}
+}
