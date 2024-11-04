dfb816393f700bbbed7dd730414f487514332f4b
Kevin Schuab <kevinschuab@gmail.com>
2024-11-04 13:05:42 -0300

    enviando a atividade 2 do dia 11 sobre stack ( pilhas )

---
+++ atividadesGen/src/atividadesGen/dia_11/Atividade2.java
@@ -0,0 +1,83 @@
+package atividadesGen.dia_11;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Atividade2 {
+
+	public static void main(String[] args) {
+
+		Stack<String> pilha = new Stack<String>();
+
+		Scanner entrada = new Scanner(System.in);
+
+		byte opcao;
+		boolean continuar = true;
+
+		do {
+			System.out.println("\n|-------------MENU--------------|");
+			System.out.println("| 1 - Adicionar livro na pilha  |");
+			System.out.println("| 2 - Listar todos os livros    |");
+			System.out.println("| 3 - Retirar livro da fila     |");
+			System.out.println("| 0 - Sair                      |");
+			System.out.println("|_______________________________|");
+
+			System.out.println("Qual a opção desejada?");
+			opcao = entrada.nextByte();
+			entrada.nextLine();
+
+			switch (opcao) {
+			case 1:
+				System.out.println("\nDigite o nome do livro:");
+				pilha.add(entrada.nextLine());
+
+				System.out.println("\nLivro adicionado na pilha!");
+				break;
+
+			case 2:
+				listagem(pilha, opcao);
+				break;
+
+			case 3:
+				listagem(pilha, opcao);
+				break;
+
+			case 0:
+				System.out.println("\nSaindo do sistema...");
+				continuar = false;
+				entrada.close();
+				break;
+
+			default:
+				System.out.println("\nOpção inválida!");
+				break;
+			}
+
+		} while (continuar);
+		System.out.println("\nSistema finalizado!");
+	}
+
+	private static void listagem(Stack<String> pilha, byte opcao) {
+
+		if (!pilha.isEmpty()) {
+			if (opcao == 3) {
+				pilha.pop();
+			}
+
+			System.out.println("\n-----------PILHA------------");
+
+			if (!pilha.isEmpty()) {
+				for (String livro : pilha) {
+					System.out.println(livro);
+				}
+			} else {
+				System.out.println("A pilha de livros acabou!");
+			}
+
+			System.out.println("----------------------------\n");
+		} else {
+			System.out.println("\nPilha vazia...");
+		}
+	}
+}
